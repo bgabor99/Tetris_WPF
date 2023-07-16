b6ef7e58f63f739c6d05558fcdc426391b061a80
Gabor Berta <bertagabor99@gmail.com>
2023-07-16 11:53:23 +0200

    Model added

---
+++ Tetris_Wpf_Program/Model/Directions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tetris_Wpf.Model
+{
+    public class Directions
+    {
+        public enum Direction { Left, Right, Rotate }
+    }
+}
